97569a419c6c5ac39549ba24dbae0b833868ef26
wangding <408542507@qq.com>
2019-03-18 12:14:10 +0800

    Grunt后端单元测试

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,40 @@
 module.exports = function (grunt) {
-  grunt.initConfig({
-        imagemin: {                               
-          files: {
-                    expand: true,
-          src: ['images/*.{png,jpg,gif}'],
-                                 dest: 'dist/'
-                                       
-          }
-              }
-                
-  });
+    grunt.initConfig({
+      mochacli: {
+        options: {
+          reporter: 'spec',
+          bail: true
+            
+        },
+          all: ['test/*.js']
+                           
+      },
+      mocha_istanbul: {
+        coverage: {
+           src: 'test'
+                    
+      }
+          
+      },
+      istanbul_check_coverage: {
+        default: {
+         options: {
+          coverageFolder: 'coverage*',
+          check: {
+             lines: 90,
+             statements: 90
+                                                  
+             }              
+          }      
+        }      
+      }   
+    });
 
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+      grunt.loadNpmTasks('grunt-mocha-cli');
+      grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-      grunt.registerTask('default', ['imagemin']);    
-
-};
+      grunt.registerTask('default', ['mochacli']);   
+      grunt.registerTask('cover', ['mocha_istanbul']);
+       grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+  };
+ 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# grunt-demo
-less预处理
+# Grunt Demo
+
+Grunt工具自动执行服务器代码单元测试的构建任务：
+即：grunt-mocha-cli插件的用法
+
+进行代码覆盖率测试：grunt cover
+查看代码覆盖率报告grunt check-cover
