04f78e6d039aff6c1593ac82bf2c5d6f41b3e2ad
wangding <408542507@qq.com>
2019-03-18 19:50:46 +0800

    Grunt 自动执行 htmlmin 任务 grunt-contrib-htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,19 @@
 module.exports = function(grunt){
   grunt.initConfig({
-    htmlhint:{
-      options:{
-        htmlhintrc:'.htmlhintrc'
-      },
-      src:['*.html']
-    }
-  });
+      htmlmin: {
+        options: {
+            collapseWhitespace: true,
+            preserveLineBreaks: false         
+        },
+        files: {
+            src: './index.html',
+            dest: 'dist/index.html'
+           }      
+      }              
+    });
 
-  grunt.loadNpmTasks('grunt-htmlhint');
+      grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  grunt.registerTask('default',['htmlhint']);
+      grunt.registerTask('default', ['htmlmin']); 
+
 };
